89334c3ef02e7c95471192cbedb1961cfa940ca5
Svetlin <svetlinst@gmail.com>
2016-07-02 11:24:14 +0300

    exam

---
+++ P20-PoolPipes/PoolPipes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class PoolPipes
+    {
+        static void Main(string[] args)
+        {
+        int poolCapacity = int.Parse(Console.ReadLine());
+        int firstPipeFlow = int.Parse(Console.ReadLine());
+        int secondPipeFlow = int.Parse(Console.ReadLine());
+        double fillHours = double.Parse(Console.ReadLine());
+        double firstPipeContribution = (firstPipeFlow * fillHours);
+        double secondPipeContribution = (secondPipeFlow * fillHours);
+        double poolCurrentState = firstPipeContribution + secondPipeContribution;
+
+        if (poolCurrentState<=poolCapacity)
+        {
+            Console.WriteLine("The pool is {0:f0}% full. Pipe 1: {1:f0}%. Pipe 2: {2:f0}%.",
+                        (int)(100 * poolCurrentState /poolCapacity),
+                        (int)(100 * firstPipeContribution /poolCurrentState),
+                        (int)(100 * secondPipeContribution /poolCurrentState));
+        }
+        else
+        {
+            Console.WriteLine("For {0} hours the pool overflows with {1:##.##} liters.",
+                            fillHours, poolCurrentState-poolCapacity);
+        }
+    }
+}
+
